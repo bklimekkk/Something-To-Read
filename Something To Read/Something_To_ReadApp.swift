fb194f09164428e89cffc7b511e64f1aea39c489
bklimekkk <klimekbartosz12@gmail.com>
2022-06-20 11:35:15 +0100

    Initial Commit

---
+++ Something To Read/Something_To_ReadApp.swift
@@ -0,0 +1,17 @@
+//
+//  Something_To_ReadApp.swift
+//  Something To Read
+//
+//  Created by Bartosz Klimek on 20/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct Something_To_ReadApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
